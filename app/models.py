24ad182c1bdef8829f728d9309573e3936db2d7f
Simon <l8tenever@gmail.com>
2025-12-22 10:55:01 +0100

    v.1.1.45

--- app/models.py
+++ app/models.py
@@ -94,3 +94,7 @@ class AuditLog(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     action = db.Column(db.String(256), nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+class Subject(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True, nullable=False)
